23762bc8df80348a7ce7acefcd0ec12217ec975d
Tianshu Ren <tianshu.r@gmail.com>
2016-08-25 23:35:59 -0400

    fixed bug

--- Scripts/lin_reg_main.m
+++ Scripts/lin_reg_main.m
@@ -5,35 +5,35 @@ load_outside_functions;
 
 subject_idx = [151 152 153 158 159 160 171 173 175 176 187 188 189 177 12 13 6 181 182 183 184 186 190 191 192 193 194 196];
 
-fprintf('loading data...')
+fprintf('loading data...\n')
 [X, y, event_types, subject_range, final_mask] = load_data(subject_idx, 0);
 X = [event_types; X]';
 y = y';
-fprintf('finished loading data')
+fprintf('finished loading data\n')
 
-fprintf('normalizing data...')
+fprintf('normalizing data...\n')
 last_idx = 1;
 for i = 1:length(subject_idx)
 	y(last_idx : subject_range(i)) = normalize(y(last_idx : subject_range(i)));
 end
-fprintf('finished normalization')
+fprintf('finished normalization\n')
 
-fprintf('partitioning into training and testing set...')
-X_train = X(1:subject_range(i), :);
-X_test = X(subject_range(i) + 1 : end, :);
-y_train = y(1:subject_range(i), :);
-y_test = y(subject_range(i) + 1 : end, :);
-fprintf('finished partition')
+fprintf('partitioning into training and testing set...\n')
+X_train = X(1:subject_range(1), :);
+X_test = X(subject_range(1) + 1 : end, :);
+y_train = y(1:subject_range(1), :);
+y_test = y(subject_range(1) + 1 : end, :);
+fprintf('finished partition\n')
 
-fprintf('training...')
+fprintf('training...\n')
 b = lin_reg_train(X_train, y_train);
-fprintf('finished training')
+fprintf('finished training\n')
 
-fprintf('predicting on test set...')
+fprintf('predicting on test set...\n')
 pred = lin_reg_pred(X_test, b);
-fprintf('finished predicting')
+fprintf('finished predicting\n')
 
-fprintf('evaluating r-sqaure')
+fprintf('evaluating r-sqaure\n')
 rsqr = r2(y_test, pred);
 
-fprintf('r_square = %0.3f', rsqr);
+fprintf('r_square = %0.3f\n', rsqr);
