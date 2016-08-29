f84113217a72576305a9cce34801745947f3ba51
Tianshu Ren <tianshu.r@gmail.com>
2016-08-29 11:42:06 -0400

    bug fix

--- Models/Elastic_net/elas_net_train.m
+++ Models/Elastic_net/elas_net_train.m
@@ -1,4 +1,4 @@
 function [b, fitinfo] = elas_net_train(X, y, params)
-	[b, fitinfo] = lasso(X, y, 'Alpha', params.alpha, 'Standardize', true, 
+	[b, fitinfo] = lasso(X, y, 'Alpha', params.alpha, 'Standardize', true, ...
 		'DFmax', params.DFmax, 'CV', params.cv_num, 'Options', params.opts);
 end
