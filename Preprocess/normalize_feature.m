41b8d362dd65e357c5cbffe33bb68ed0bd21f787
Tianshu Ren <tianshu.r@gmail.com>
2016-09-16 19:40:57 -0400

    fixed bug

--- Preprocess/normalize_feature.m
+++ Preprocess/normalize_feature.m
@@ -1,7 +1,7 @@
 function [res] = normalize_feature(X)
 	% normalize a n * p matrix so that each observation of a feature is transformed into the respective z-score
 	m = mean(X);
-	s = std(X);
 	res = bsxfun(@minus, X, m);
+	s = std(res);
 	res = bsxfun(@rdivide, res, s);
 end
