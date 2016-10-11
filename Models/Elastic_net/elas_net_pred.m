3de42b495457b1c65c1b060385f53fe04433c283
Tianshu Ren <tianshu.r@gmail.com>
2016-10-11 13:56:00 -0400

    sth

--- Models/Elastic_net/elas_net_pred.m
+++ Models/Elastic_net/elas_net_pred.m
@@ -1,5 +1,5 @@
 % function [pred] = elas_net_pred(X, b)
-function [pred] = elas_net_pred(X, model)
-	% pred = lin_reg_pred(X, b);
-	pred = cvglmnetPredict(model, X);
+% function [pred] = elas_net_pred(X, model)
+	pred = lin_reg_pred(X, b);
+	% pred = cvglmnetPredict(model, X);
 end
