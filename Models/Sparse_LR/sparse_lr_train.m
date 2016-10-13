4418b173f6d41762b21ea12e47b8c696b30120e4
Tianshu Ren <tianshu.r@gmail.com>
2016-10-12 21:19:14 -0400

    parpool

--- Models/Sparse_LR/sparse_lr_train.m
+++ Models/Sparse_LR/sparse_lr_train.m
@@ -1,6 +1,11 @@
 function [model] = sparse_lr_train(X, y, params)
-	options = glmnetSet;
-	options.alpha = params.alpha;
-	options.dfmax = params.dfmax;
-	model = cvglmnet(X, y, 'multinomial', options, [], params.cv_num, [], true);
+	[nx, px] = size(X);
+	[ny, py] = size(y);
+	assert(nx == ny, 'invalid input: number of observations does not match.');
+
+	[b, stats] = lassoglm(X, y, 'binomial', 'Alpha', params.alpha, 'CV', params.cv_num, 'Options', params.opts, 'DFmax', 'params.dfmax')
+	% options = glmnetSet;
+	% options.alpha = params.alpha;
+	% options.dfmax = params.dfmax;
+	% model = cvglmnet(X, y, 'multinomial', options, [], params.cv_num, [], true);
 end
