e2ee2b7871566d070a904b32af7a8a3a83d3ad44
Tianshu Ren <tianshu.r@gmail.com>
2016-10-12 21:57:55 -0400

    corr analysis

---
+++ Models/Correlation_VS/cor_pred.m
@@ -0,0 +1,4 @@
+function [pred] = cor_pred(X, model)
+	Xn = X(:, model.idx);
+	pred = lin_reg_pred(Xn, model.b);
+end
---
+++ Models/Correlation_VS/cor_train.m
@@ -0,0 +1,8 @@
+function [model] = cor_train(X, y, params)
+	cr = corr(X, y);
+	[~, ord] = sort(cr, 1, 'descend');
+	idx = ord(1:params.DFmax);
+	Xn = X(:, idx);
+	model.b = lin_reg_train(Xn, y, {});
+	model.idx = idx;
+end
