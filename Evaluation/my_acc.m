ebd80e51654c3775e3205dac462770790ed54435
Tianshu Ren <tianshu.r@gmail.com>
2016-10-19 00:51:09 -0400

    bug

--- Evaluation/my_acc.m
+++ Evaluation/my_acc.m
@@ -1,4 +1,5 @@
 function [score] = my_acc(truth, prediction)
-	[~, prediction] = max(prediction, [], 2);
+	% [~, prediction] = max(prediction, [], 2);
+	prediction = prediction > 0.5;
 	score = sum(truth == prediction) / numel(truth);
 end
