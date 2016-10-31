d2efcc392d4881673fb8d0bb14bbe60637d7fd36
Tianshu Ren <tianshu.r@gmail.com>
2016-10-30 22:47:31 -0400

    bug fix

--- Models/Pca/pca_train.m
+++ Models/Pca/pca_train.m
@@ -3,13 +3,14 @@ function [model] = pca_train(X, y, param)
 	tmp = 0;
 	pc_num = 0;
 	for i = 1 : length(explained)
-		tmp = tmp + explained(i);
+		tmp = tmp + explained(i) / 100;
 		pc_num = pc_num + 1;
 		if (tmp > param.pc_explained)
 			break
 		end
 	end
 	coeff = coeff(:, 1:pc_num);
+    score = score(:, 1:pc_num);
 	b = lin_reg_train(score, y);
 	model = struct;
 	model.b = b;
