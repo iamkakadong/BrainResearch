250be7c89cc6830ab8b61cfadd9d82451e17d244
Tianshu Ren <tianshu.r@gmail.com>
2016-12-01 19:50:13 -0500

    bug

--- Scripts/New_data/config.m
+++ Scripts/New_data/config.m
@@ -1,3 +1,5 @@
+addpath(genpath('~/BrainResearch/'));
+
 sub_idxs = [0464  0551  0553  0560  0581  0595  0549 ...
 			0552  0554  0566  0585  0591  0596  0609];
 
@@ -9,4 +11,3 @@ for i = 1:8
 	tmp = (56 + 6) * (i - 1) + 1;
 	trial_idxs = [trial_idxs, tmp : tmp + 56];
 end
-
