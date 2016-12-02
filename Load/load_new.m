bc132cdc6d01c37c4deccef9aefdc73b7f7ba548
Tianshu Ren <tianshu.r@gmail.com>
2016-12-01 19:42:43 -0500

    new data

---
+++ Load/load_new.m
@@ -0,0 +1,47 @@
+function [data] = load_data(sub_idxs, trial_idxs)
+	% data is a cell array of length equal to length of sub_idxs
+	% each cell contains:
+	% 	X: the feature matrix (n * p)
+	%	y: the response vector (n * 1)
+	%	c: the condition matrix (n * d)
+	p_dir = '/data2/tren/NewData/';
+	data = cell(length(sub_idxs), 1);
+	% X = [];
+	% y = [];
+	% c = [];
+
+	for idx = sub_idxs
+		res = load_response(p_dir, idx);
+		data{idx}.y = res.y;
+		data{idx}.c = res.c;
+		data{idx}.X = load_features(p_dir, idx, trial_idxs);
+		% y = [y; res.y];
+		% c = [c; res.c];
+		% X = [X; load_features(p_dir, idx, trial_idxs)];
+	end
+end
+
+function [res] = load_response(p_dir, idx)
+	% res.y is n * 1
+	% res.c is n * d, where d is the number of conditions
+	res = struct;
+	res.y = [];
+	res.c = [];
+	for i = 1:8
+		tmp = importdata(strcat(p_dir, num2str(idx, '%04d'), '/DangerZone_run', num2str(i, '%01d'), '/'));
+		res.y = [res.y; tmp(:, 1)];
+		res.c = [res.c; tmp(:, 2:end)];
+	end
+end
+
+function [res] = load_features(p_dir, idx, trial_idxs)
+	% res is a n * p matrix with invalid entries removed and features standardized
+	res = [];
+	for i = trial_idxs
+		tmp = load_untouch_nii(strcat(p_dir, num2str(idx, '%04d'), '/single_trial_GLM/beta_', num2str(i, '%04d'), '.nii'));
+		tmp = tmp.img(:);
+		tmp = tmp(~isnan(tmp))';
+		res = [res; tmp];
+	end
+	res = normalize_feature(res);
+end
---
+++ Scripts/New_data/data_specs.m
@@ -0,0 +1,12 @@
+sub_idx = [0464  0551  0553  0560  0581  0595  0549 ...
+			0552  0554  0566  0585  0591  0596  0609];
+
+train_subs = sub_idx(1 : 10);
+test_subs = sub_idx(11 : 14);
+
+trial_idxs = [];
+for i = 1:8
+	tmp = (56 + 6) * (i - 1) + 1;
+	trial_idxs = [trial_idxs, tmp : tmp + 56];
+end
+
