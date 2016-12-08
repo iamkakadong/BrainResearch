bca11f499fc04b1f3cc950d611ac8d676d8289fe
Tianshu Ren <tianshu.r@gmail.com>
2016-12-08 13:46:00 -0500

    new

--- Scripts/New_data/corr_vs.m
+++ Scripts/New_data/corr_vs.m
@@ -12,17 +12,17 @@ data = load_new(train_subs, trial_idxs, vmask);
 tmp = struct;
 tmp.X = [];
 tmp.y = [];
+subject_range = [];
 
 for i = 1:length(data)
 	tmp2 = [normalize_feature(data{i}.c), data{i}.X];
 	tmp.X = [tmp.X; tmp2];
 	tmp.y = [tmp.y; data{i}.y];
+	subject_range = [subject_range, length(data{i}.y)];
 end
 
-tmp.X = tmp.X(idx, :);  % idx is a vector of indices excluding outliers
-tmp.y = tmp.y(idx, :);
 data = tmp;
-% subject_range = [56 * 8 : 56 * 8 : 56 * 8 * length(train_subs)];
+subject_range = cumsum(subject_range);
 
 DFmax = [1, 5, 15, 50, 250, 500, 1000];
 % l_alpha = [0.9, 0.1, 0.01, 0.001];
