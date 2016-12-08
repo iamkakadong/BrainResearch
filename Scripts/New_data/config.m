ecd747ac403389d8bb11c00e588a08d95862b72d
Tianshu Ren <tianshu.r@gmail.com>
2016-12-08 13:03:00 -0500

    new

--- Scripts/New_data/config.m
+++ Scripts/New_data/config.m
@@ -1,11 +1,11 @@
 addpath(genpath('~/BrainResearch/'));
 load('vmask.mat');
 
-sub_idxs = [0464  0551  0553  0560  0581  0595  0549 ...
+sub_idxs = [0464  0551  0553  0560  0581  0595  0549  0555  0556  0567  0568  0590  0606...
 			0552  0554  0566  0585  0591  0596  0609];
 
-train_subs = sub_idxs(1 : 10);
-test_subs = sub_idxs(11 : 14);
+train_subs = sub_idxs(1 : 12);
+test_subs = sub_idxs(13 : 16);
 
 trial_idxs = [];
 for i = 1:8
