3dc7e5a677339967eae2ce95767605f1aaf28647
Tianshu Ren <tianshu.r@gmail.com>
2017-01-27 15:04:06 +0800

    dangerzone data

---
+++ danger_zone/build_mask.m
@@ -0,0 +1,14 @@
+
+config;
+
+p_dir = '/data2/tren/NewData/';
+vmask = ones(79 * 95 * 78, 1);
+for i = sub_idxs
+	for j = trial_idxs
+		tmp = load_untouch_nii(strcat(p_dir, num2str(i, '%04d'), '/single_trial_GLM/beta_', num2str(j, '%04d'), '.nii'));
+		tmp = tmp.img(:);
+		vmask = vmask & isnan(tmp);
+	end
+end
+
+save('mask.mat', 'vmask');
---
+++ danger_zone/config.m
@@ -0,0 +1,14 @@
+addpath(genpath('~/BrainResearch/'));
+load('vmask.mat');
+% load('ol_range.mat');
+
+sub_idxs = [0464  0551  0553  0560  0581  0595  0549  0555  0556  0567  0568  0590  0606  0552  0554  0566  0585  0591  0596  0609];
+
+train_subs = sub_idxs(1 : 15);
+test_subs = sub_idxs(16 : 20);
+
+trial_idxs = [];
+for i = 1:8
+	tmp = (56 + 6) * (i - 1) + 1;
+	trial_idxs = [trial_idxs, tmp : tmp + 55];
+end
